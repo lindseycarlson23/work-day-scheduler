4644ae28ae672ed1ea4ca54fbdbc1c754e9d793f
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-10-07 22:01:39 -0500

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# work-day-scheduler
+# **Work Day Scheduler**
+
+## **Project Overview**
+
+This project is a basic scheduling tool for planning your work day by the hour. You are able to click on the box next to each hour and enter your task for that hour. When you return to the site later in the day, it will still show what you entered.
+
+In this project, I focused on using jQuery to add dynamic functionality. I also used dayjs for the time and date information.
+
+## **Installation**
+
+N/A
+
+## **Usage**
+
+Anyone can use this site and enter their schedule for the day.
+
+## **Credits**
+
+Bootcamp starter code
+
+
+## **License**
+
+Please refer to the license in the repo.
--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-//     // TODO: Add a listener for click events on the save button. This code should
-//     // use the id in the containing time-block as a key to save the user input in
-//     // local storage. HINT: What does `this` reference in the click listener
-//     // function? How can DOM traversal be used to get the "hour-x" id of the
-//     // time-block containing the button that was clicked? How might the id be
-//     // useful when saving the description in local storage?
-//     //
-//     // TODO: Add code to apply the past, present, or future class to each time
-//     // block by comparing the id to the current hour. HINTS: How can the id
-//     // attribute of each time-block be used to conditionally add or remove the
-//     // past, present, and future classes? How can Day.js be used to get the
-//     // current hour in 24-hour time?
-//     //
-//     // TODO: Add code to get any user input that was saved in localStorage and set
-//     // the values of the corresponding textarea elements. HINT: How can the id
-//     // attribute of each time-block be used to do this?
-//         //localStorage
-//     //
-//     // DONE: Add code to display the current date in the header of the page.
-//       //USE DAY.JS
-
-//   });
-  
+ 
 
 // handle displaying the time
 var dateDisplayElem = $("#currentDay");
@@ -37,6 +11,7 @@ function displayDate() {
 displayDate();
 
 
+//color-codes the hours based on whether they are past, present, or future
 function updateCalendar(){
   $('#calendarContainer').children('div').each((index,item)=> {
     var hour = parseInt($(item).attr("id").split("-")[1]);
@@ -65,7 +40,7 @@ function updateCalendar(){
 };
 
 
-
+//save button event listener
 $(document).ready(function() {
   $('button').on('click', function() {
     var parentId = $(this).parent().attr("id");
@@ -78,6 +53,7 @@ $(document).ready(function() {
 
 });
 
+//load any stored text in time-blocks when page loads
 function loadCalendar() {
   for (var key in localStorage){
     console.log(key)
@@ -85,30 +61,8 @@ function loadCalendar() {
       $("#" + key).children('textarea')[0].value=localStorage.getItem(key);
     }
  }
-}
+};
 
-
-// $("hourTime-9 .textArea").val(localStorage.getItem("note"));
-// var storedNote =JSON.parse(localStorage.getItem('note'));
-// console.log(storedNote)
-
-//how do I get all the values to load and go to the right place
-//how do I get them to store as an array
-// $("hourTime-9").textArea.value = storedNote; //put in chat GPT and ask
-
-// $(document).ready(function () {
-//   var storedNote = localstorage.getItem('hourtime-9');
-//   if ()
-// })
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-//     // use the id in the containing time-block as a key to save the user input in
-//     // local storage. HINT: What does `this` reference in the click listener
-//     // function? How can DOM traversal be used to get the "hour-x" id of the
-//     // time-block containing the button that was clicked? How might the id be
-//     // useful when saving the description in local storage?
 
 
 
